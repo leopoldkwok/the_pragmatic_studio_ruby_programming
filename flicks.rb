2b8014e8008dee48142490f2022d9ec1c4e5d9f6
leopoldkwok <leopoldkwok@gmail.com>
2015-05-23 07:30:03 +0100

    session 7 - classes

--- flicks.rb
+++ flicks.rb
@@ -23,20 +23,61 @@
 
 # a method is like a black box
 
-def weekday
-	current_time = Time.new
-	today = current_time.strftime("%A")
+# def weekday
+# 	current_time = Time.new
+# 	today = current_time.strftime("%A")
+# end
+
+# def movie_listing(title, rank=0)
+# 	"#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}."
+# end
+
+
+
+# puts movie_listing("goonies", 10)
+# puts movie_listing("ghostbusters, 9")
+
+# a_title = "goldfinger" #local variable
+# puts movie_listing(a_title)
+
+class Movie
+	def initialize(title, rank=0)
+		@title = title.capitalize
+		@rank = rank
+		# puts "Created a movie object with title #{title} and rank #{rank}"
+	end
+
+	def thumbs_up
+		@rank +=1
+	end
+
+	def thumbs_down
+		@rank -=1
+		# @rank = @rank - 1 same as above
+	end
+
+	def to_s
+		"#{@title} has a rank of #{@rank}"
+	end
 end
 
-def movie_listing(title, rank=0)
-	"#{weekday.upcase}: #{title.capitalize} has a rank of #{rank}."
-end
+movie1 = Movie.new("goonies", 10)
+movie1.thumbs_up
+puts movie1
+
+movie2 = Movie.new("ghostbusters", 9)
+movie2.thumbs_down
+puts movie2
 
 
+movie3 = Movie.new("goldfinger")
+puts movie3
 
-puts movie_listing("goonies", 10)
-puts movie_listing("ghostbusters, 9")
 
-a_title = "goldfinger" #local variable
-puts movie_listing(a_title)
+# movie2 = Movie.new("ghostbusters", 9)
 
+# movie1 = Movie.new
+# puts movie1.object_id
+
+# movie2 = Movie.new
+# puts movie2.object_id
