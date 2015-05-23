00d9121497d99730f08b817e39441221a5572178
leopoldkwok <leopoldkwok@gmail.com>
2015-05-23 09:56:46 +0100

    session 10 - objects interacting

--- flicks.rb
+++ flicks.rb
@@ -111,15 +111,59 @@ end
 
 # Session 9 Arrays
 
+# movie1 = Movie.new("goonies", 10)
+# movie2 = Movie.new("ghostbusters", 9)
+# movie3 = Movie.new("goldfinger")
+
+# movies = [movie1, movie2, movie3]
+
+# puts movies
+
+# movies.each do |m|
+# 	m.thumbs_up
+# 	puts m
+# end
+
+# session 10 - objects interacting
+
+class Playlist
+	def initialize(name)
+		@name = name
+		@movies = []
+	end
+
+	def add_movie(movie)
+		@movies << movie
+	end
+
+	def play
+		puts "#{@name}'s playlist:"
+		puts @movies
+		@movies.each do|movie|
+			movie.thumbs_up
+			puts movie
+		end
+
+	end
+
+end
+
+
 movie1 = Movie.new("goonies", 10)
 movie2 = Movie.new("ghostbusters", 9)
 movie3 = Movie.new("goldfinger")
 
-movies = [movie1, movie2, movie3]
+playlist1 = Playlist.new("Kermit")
 
-puts movies
+playlist1.add_movie(movie1)
+playlist1.add_movie(movie2)
+playlist1.add_movie(movie3)
 
-movies.each do |m|
-	m.thumbs_up
-	puts m
-end
+playlist1.play
+
+playlist2 = Playlist.new("Fozzie")
+playlist2.add_movie(movie3)
+
+movie4 = Movie.new("gremlins", 15)
+playlist2.add_movie(movie4)
+playlist2.play
