bf986e2abd1b79ad06e953e642845ff417c5b855
leopoldkwok <leopoldkwok@gmail.com>
2015-05-31 23:59:19 +0100

    more progress with iterators

--- iterators.rb
+++ iterators.rb
@@ -1,9 +1,42 @@
-def three_times
-	puts "Ready"
-	yield
-	yield
-	puts "After yield"
-end
+numbers = (1..10).to_a
+# puts numbers.select {|n| n.even? }
 
-#yield passes control to the block below
-three_times { |number| puts number}
+def my_select(array)
+	results = []
+	array.each do |element|
+		results << element if yield(element) 
+	end
+	results 
+end  
+
+puts my_select(numbers) {|n| n.even? }
+
+
+
+# def compute
+# 	if block_given?
+# 		puts yield
+# 	else
+# 		puts "Does not compute!"
+# 	end
+# end
+
+# compute {3.14}
+# do
+# 	"Hello"
+# 	7
+# 	3.14
+# end
+
+
+# def three_times
+# 	puts "Ready"
+# 	yield(1)
+# 	puts "Set"
+# 	yield 2
+# 	puts "Go!"
+# 	yield 3  
+# end
+
+# #yield passes control to the block below
+# three_times { |number| puts number * number }
